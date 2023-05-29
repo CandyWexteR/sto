be6b392e97daea1c840b519d25771f1fee5b1fe7
malai <malai@plant.elesy.com>
2023-05-29 14:05:34 +0700

    Основа запроса заявок

---
+++ Application/Orders/Queries/GetPageOrders/GetPageOrders.cs
@@ -0,0 +1,10 @@
+﻿using Application.CQRS;
+using Application.PagedResult;
+using Core.Models.Orders;
+
+namespace Application.Orders.Queries.GetPageOrders;
+
+public class GetPageOrders : IQuery<PagedResult<Order>>
+{
+    
+}
---
+++ Application/Orders/Queries/GetPageOrders/OrderViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Orders.Queries.GetPageOrders;
+
+public class OrderViewModel
+{
+    public OrderViewModel Type { get; set; }
+}
