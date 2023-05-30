46e4745159617105dc65443ae2d2eb964f3d1718
shoot <mr.fostik@mail.ru>
2023-05-30 20:03:53 +0700

    Добавлены запросы на ордеры

--- Application/Orders/Queries/GetPageOrders/GetPageOrdersHandler.cs
+++ Application/Orders/Queries/GetPageOrders/GetPageOrdersHandler.cs
@@ -1,13 +1,32 @@
 ﻿using Application.CQRS;
 using Application.Orders.ViewModels;
 using Application.PagedResult;
+using Core.Repositories;
 
 namespace Application.Orders.Queries.GetPageOrders;
 
 public class GetPageOrdersHandler : IQueryHandler<GetPageOrders, PagedResult<OrderViewModel>>
 {
-    public Task<PagedResult<OrderViewModel>> Handle(GetPageOrders request, CancellationToken cancellationToken)
+    private readonly IOrdersRepository _orders;
+
+    public GetPageOrdersHandler(IOrdersRepository orders)
     {
-        throw new NotImplementedException();
+        _orders = orders;
+    }
+
+    public async Task<PagedResult<OrderViewModel>> Handle(GetPageOrders request, CancellationToken cancellationToken)
+    {
+        var all = await _orders.GetAllAsync();
+        return all.Select(order => new OrderViewModel()
+        {
+            Id = order.Id,
+            Description = order.Description,
+            Title = order.Title,
+            CreatedAt = order.CreatedAt,
+            OrderDelivered = order.OrderDelivered,
+            ResponsibleUserId = order.ResponsibleUserId,
+            OrderItems = new List<OrderedItemViewModel>()
+        })
+            .ToPagedResult(request);
     }
 }
